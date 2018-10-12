e62484fca52f7dc3776c1f722e0625b68b54e9a7
abimaelhernandez <hernandezabimael364@gmail.com>
2018-10-12 15:37:42 -0500

    empezando trial para pagos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React,{Component} from 'react';
+import axios from 'axios';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      list : [1,2,3,4,5,6,7,8,9]
+    }
+  }
+
+componentDidMount(){
+ console.log(this.state.list);
+}
+
+sendInfo = () => {
+  axios.post('ttps://factura.com/api/v1/clients/create', {
+
+  })
+}
+
   render(){
     return (
-      <h1>hola mundo</h1>
+      <div className="allInputs">
+        <input className="input"/><br/>
+        <input className="input"/><br/>
+        <input className="input"/><br/>
+        <input className="input"/><br/>
+        <input className="input"/><br/>
+        <input className="input"/><br/>
+        <input className="input"/><br/>
+        <input className="input"/><br/>
+        <input className="input"/>
+        <div><button  className="actionButton" onClick={this.sendInfo}> send </button></div>
+      </div>
     )
   }
 }
--- server/config.js
+++ server/config.js
@@ -1,7 +1,24 @@
 const express = require("express");
+//const querystring = require('querystring');
+const request = require('request');
 
 const app = express();
 
 app.use(express.static(__dirname + "/../client/dist"));
 
+//headers for post to factura.com here
+req.post({
+   url: 'someUrl',
+   form: { email: 'user', razons: '', rfc: '', calle: '', numero_exterior: '', codpos: '', colonia: '', estado:'', ciudad:'' },
+   headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36',
+      'Content-Type' : 'application/x-www-form-urlencoded'
+   },
+   method: 'POST'
+  },
+
+  function (e, r, body) {
+      console.log(body);
+  });
+//export your app
 module.exports = app;
