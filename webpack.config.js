75f260b0399d046e8e85c964f0fc7bc2eed0b44b
abimaelhernandez <hernandezabimael364@gmail.com>
2018-10-12 10:51:57 -0500

    finished the boiler plate

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './client/src/Index.js'
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'client/dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: [
+          'babel-loader'
+        ],
+        exclude: /node_modules|packages/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
