6d5bb5a4fa16d2f96192026a9c9e8f4b0565b197
abimaelhernandez <hernandezabimael364@gmail.com>
2018-10-15 18:28:05 -0500

    the post request is now working and creating the clients for invoices

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,50 +5,54 @@ export default class App extends Component {
   constructor(){
     super()
     this.state = {
-      list : [1,2,3,4,5,6,7,8,9]
+      list : []
     }
   }
 
 componentDidMount(){
  console.log(this.state.list);
 }
-
+//function to send all the info about the client
 sendInfo = () => {
-  axios.post('https://factura.com/api/v1/clients/create', {
-    "nombre": "Abimael",
-    "apellidos": "Hernandez Tapia",
-    "email": "htapia.abi14@gmail.com",
-    "razons": "Abimael Hernandez",
-    "rfc": "XAXX010101000",
-    "calle": "Av. Juarez",
-    "numero_exterior": 1234,
-    "numero_interior": "",
-    "codpos": 44640,
-    "colonia": "Centro",
-    "estado": "Jalisco",
-    "ciudad": "Guadalajara",
-    "delegacion": ""
+  axios({
+    method : 'post',
+    url:'http://devfactura.in/api/v1/clients/create',
+    data : {
+      "nombre": "Abimael",
+      "apellidos": "Hernandez Tapia",
+      "email": "htapia.abi14@gmail.com",
+      "razons": "Abimael Hernandez",
+      "rfc": "HETA921202EF8",
+      "calle": "Av. Juarez",
+      "numero_exterior": "12",
+      "codpos": "44640",
+      "colonia": "Centro",
+      "estado": "Jalisco",
+      "ciudad": "Guadalajara",
+      "delegacion": ""
+    },
+    //this are the headers to allow the user to be created
+    headers: {
+      'Content-Type': 'application/json',
+      'F-API-KEY': 'JDJ5JDEwJFJvbEFwQjdXNTZXMlJWbUl4MmNlYi5ueG5RNURhZVkvSFladGpZSU5HNk5BM1VVLzBQSkJt',
+      'F-SECRET-KEY': 'JDJ5JDEwJE5tZzN0R3JjVWxPZFJFRVd6SzZBYXV5U0VCcTZ3dGhtd3huOVNjbmFXcmZsVzRBUXBwdkZD'
+    }
   })
   .then(res => {
    console.log('hello there here are results',res);
+   this.setState({list: res})
+   console.log('THIS IS YOUR ACTUAL STATE',this.state.list);
   })
   .catch(error => {
     console.log('ooop dude theres an error some where', error);
   })
 }
 
+
+
   render(){
     return (
       <div className="allInputs">
-        <input className="input"/><br/>
-        <input className="input"/><br/>
-        <input className="input"/><br/>
-        <input className="input"/><br/>
-        <input className="input"/><br/>
-        <input className="input"/><br/>
-        <input className="input"/><br/>
-        <input className="input"/><br/>
-        <input className="input"/>
         <div><button  className="actionButton" onClick={this.sendInfo}> send </button></div>
       </div>
     )
--- server/config.js
+++ server/config.js
@@ -7,15 +7,15 @@ app.use(express.static(__dirname + "/../client/dist"));
 
 //headers for post to factura.com
 
-// Set the headers
+//Set the headers
 const headers = {
     'Content-Type': 'application/json',
     'F-API-KEY': 'JDJ5JDEwJFJvbEFwQjdXNTZXMlJWbUl4MmNlYi5ueG5RNURhZVkvSFladGpZSU5HNk5BM1VVLzBQSkJt',
     'F-SECRET-KEY': 'JDJ5JDEwJE5tZzN0R3JjVWxPZFJFRVd6SzZBYXV5U0VCcTZ3dGhtd3huOVNjbmFXcmZsVzRBUXBwdkZD'
 }
-// Configure the request
+//Configure the request
 let options = {
-    url: 'https://factura.com/api/v1/clients/create',
+    url: 'http://devfactura.in/api/v1/clients/create',
     method: 'POST',
     headers: headers
 }
@@ -24,7 +24,7 @@ let options = {
 request(options, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         // Print out the response body
-        console.log('THIS,IS BODY OF',body)
+        console.log('THIS,IS BODY OF',body, headers)
     }
 })
 
