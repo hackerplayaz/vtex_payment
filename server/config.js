4f42df73c25cdf6be7c718efb3c5239e365102c1
jorgel <jorge@jorgelerma.com>
2018-10-15 22:26:50 -0500

    Adds required fields to request

--- server/config.js
+++ server/config.js
@@ -14,17 +14,32 @@ const headers = {
     'F-SECRET-KEY': 'JDJ5JDEwJE5tZzN0R3JjVWxPZFJFRVd6SzZBYXV5U0VCcTZ3dGhtd3huOVNjbmFXcmZsVzRBUXBwdkZD'
 }
 //Configure the request
+
+var myinfo = { email: "johnsf3@hotmail.com",
+  razons: "testiing",
+  rfc: "GOSA850313TA3",
+  calle: "Reforma 100",
+  numero_exterior: "300",
+  codpos: "06600",
+  colonia: "La paz",
+  estado: "Guerrero",
+  ciudad: "Chilpancingo"
+};
+
 let options = {
     url: 'http://devfactura.in/api/v1/clients/create',
     method: 'POST',
-    headers: headers
+    headers: headers,
+    body : JSON.stringify(myinfo)
 }
 
 // Start the request
 request(options, function (error, response, body) {
     if (!error && response.statusCode == 200) {
+      console.log(response.statusCode);
+      console.log(JSON.parse(body))
         // Print out the response body
-        console.log('THIS,IS BODY OF',body, headers)
+        // console.log('THIS,IS BODY OF',body, headers)
     }
 })
 
